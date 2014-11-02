7070ea729d286be326050d32a995ab6e77fb92ea
Jason <quizathaderat@yahoo.com>
2014-11-02 13:39:56 -0800

    Cleaned up show_comments button fuctionality, and added test.

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -4,21 +4,15 @@ $(function(){
     $(document).on('click', '.comment_closer', function(){
       $('#comment_form').remove();
       $("#comment_button-" + this.id).show();
+      $('#show_comments-' + this.id).show();
     });
-  
-  
-  	
-  
-   
-    
-    
+     
     $(document).on('click', '.show_comments', function(){
-      $('#comment_list-' + this.id).show();
-      $('#' + this.id).hide();
+      var entry_id = /\d+/.exec(this.id);
+      $('#comment_list-' + entry_id).show();
+      $('#show_comments-' + entry_id).hide();
     });
-    
-   
-    
+     
   });
 }());
   
--- spec/features/creating_comments_spec.rb
+++ spec/features/creating_comments_spec.rb
@@ -6,13 +6,13 @@
       FactoryGirl.create(:entry, title: 'Hi', content: 'There')
       visit '/'
       click_link 'blog'
-    end
-
-    scenario 'can create comment' do
       click_link 'Sign in'
       fill_in 'Email', with: 'user@test.com'
       fill_in 'Password', with: 'password'
       click_button 'Log in'
+    end
+
+    scenario 'can create comment' do
       click_link 'Comment'
       fill_in 'comment_content', with: 'interesting'
       click_button 'Create Comment'
@@ -22,14 +22,21 @@
     end
     
     scenario 'can close comment form' do
-      click_link 'Sign in'
-      fill_in 'Email', with: 'user@test.com'
-      fill_in 'Password', with: 'password'
-      click_button 'Log in'
       click_link 'Comment'
       page.find(".comment_closer").click
       expect(page).not_to have_css 'comment_form'
     end
+    
+    scenario 'comments hidden till show_comments button clicked' do
+      click_link 'Comment'
+      fill_in 'comment_content', with: 'testing testing'
+      click_button 'Create Comment'
+      visit '/entries'
+      expect(page).to have_selector("#comment_list-1", visible: false)
+      click_button 'show_comments-1'
+      expect(page).to have_selector("#comment_list-1", visible: true)
+    end
+    
     
 
   end
